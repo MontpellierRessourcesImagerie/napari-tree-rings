eb7a763205c4452ef594461488d4a2d1820c1518
Thi-Thu-Khiet Dang@ <Thi-Thu-Khiet_Dang@etu.u-bourgogne.fr>
2025-08-25 17:44:29 +0200

    Update tests

--- src/napari_tree_rings/_tests/test_widget.py
+++ src/napari_tree_rings/_tests/test_widget.py
@@ -7,13 +7,21 @@ from napari_tree_rings._widget import (
     SegmentRingsOptionsWidget
 )
 from napari_tree_rings._tests import utils
+import napari
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
+os.environ["PYVISTA_OFF_SCREEN"] = "true"
+os.environ["LIBGL_ALWAYS_SOFTWARE"] = "1"   # for Linux/macOS
+os.environ["QT_OPENGL"] = "software"        # for Windows/Qt
 
 
 
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_segment_trunk_widget(make_napari_viewer, tmp_path):
+def test_segment_trunk_widget(qtbot, tmp_path):
     # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
+    viewer = napari.Viewer()
+    qtbot.addWidget(viewer.window._qt_window)
+    assert viewer.layers == []
+
     img = utils.make_checkboard((1000, 1000), 1, 'grayscale')
 
     # Save it to a temporary TIFF file
@@ -52,8 +60,9 @@ def test_segment_trunk_widget(make_napari_viewer, tmp_path):
 
 #     assert my_widget is not None
 
-def test_segment_ring_options(make_napari_viewer):
-    viewer = make_napari_viewer()
+def test_segment_ring_options(qtbot):
+    viewer = napari.Viewer()
+    qtbot.addWidget(viewer.window._qt_window)
 
     my_widget = SegmentRingsOptionsWidget(viewer)
     
@@ -62,8 +71,9 @@ def test_segment_ring_options(make_napari_viewer):
     assert my_widget is not None
 
 
-def test_segment_trunk_options(make_napari_viewer):
-    viewer = make_napari_viewer()
+def test_segment_trunk_options(qtbot):
+    viewer = napari.Viewer()
+    qtbot.addWidget(viewer.window._qt_window)
 
     my_widget = SegmentTrunkOptionsWidget(viewer)
     
